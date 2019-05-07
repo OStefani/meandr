8cc77f8023a74aa009cb5913933a70b869e060d0
OStefani <ostefanishina@gmail.com>
2019-05-07 21:03:56 +0300

    Remove lazyloading

--- js/index.js
+++ js/index.js
@@ -33,30 +33,7 @@ else {
     }, 500);
 }
 
-// Lazy images
-let lazyImages = document.querySelectorAll('.lazy');
-if ("IntersectionObserver" in window) {
-    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
 
-      entries.forEach(function(entry) {
-
-        if (entry.isIntersecting) {
-          let lazyImage = entry.target;
-
-          lazyImage.src = lazyImage.dataset.src;
-          lazyImage.classList.remove("lazy");
-          lazyImageObserver.unobserve(lazyImage);
-        }
-      });
-    });
-
-    lazyImages.forEach(function(lazyImage) {
-      lazyImageObserver.observe(lazyImage);
-    });
-  } else {
-      let attr = lazyImages[0].getAttribute('data-src');
-    lazyImages[0].src = attr;
-  }
 
 })
 
