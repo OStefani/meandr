d42b158a58831f766baa0eea54c06544ec27ac12
OStefani <ostefanishina@gmail.com>
2019-05-07 15:27:28 +0300

    Fixed regular expression

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
 let input = document.getElementById('phone');
 let im = new Inputmask("+7(999)-999-99-99");
 im.mask(input);
-im.isValid('+7(999)-999-99-99')
+//im.isValid('+7(999)-999-99-99')
 
 document.getElementById('form').addEventListener('submit', function(e) {
     let phoneLabel = document.getElementsByClassName('phone-label')[0];
     //var isValid = Inputmask.isValid(input.value);
     e.preventDefault();
-    let regex = /^[+7]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+    let regex = /^[+7]+[(]{1}[0-9]{3}[)]{1}-\d{3}-\d{2}-\d{2}$/;
     if (!regex.test(input.value)) {
         input.classList.add('section-4__input_red');
         if (!phoneLabel.classList.contains('phone-label_show')) phoneLabel.classList.add('phone-label_show');
